2fcdf853608f9e23e349bec8d2b7b7736ba8baed
Nikola Lawniczak <nikola.lawniczak1991@gmail.com>
2026-07-28 17:30:31 +0200

    Add Fantasy Realms Vol.1 collection

--- app/src/main/java/com/example/wallora/repository/collections/FantasyRealmsCollection.kt
+++ app/src/main/java/com/example/wallora/repository/collections/FantasyRealmsCollection.kt
@@ -43,5 +43,58 @@ object FantasyRealmsCollection {
             badge = "NEW",
             isTopPick = true
         ),
+        Wallpaper(
+            name = "Dragon's Keep",
+            image = R.drawable.fantasy_castle_05,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Floating Kingdom",
+            image = R.drawable.fantasy_castle_06,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Mystic Waterfall",
+            image = R.drawable.fantasy_castle_07,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Ancient Wizard Tower",
+            image = R.drawable.fantasy_castle_08,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),Wallpaper(
+            name = "Butterfly Kingdom",
+            image = R.drawable.fantasy_castle_09,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),Wallpaper(
+            name = "Celestial Citadel",
+            image = R.drawable.fantasy_castle_10,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+
     )
 }
