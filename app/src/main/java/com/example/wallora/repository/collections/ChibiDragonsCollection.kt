6a0056659b49c3cee914ec149dfb69b11e27a3a6
Nikola Lawniczak <nikola.lawniczak1991@gmail.com>
2026-07-30 09:49:41 +0200

    Add Chibi Dragons Vol. 1 wallpaper collection

--- app/src/main/java/com/example/wallora/repository/collections/ChibiDragonsCollection.kt
+++ app/src/main/java/com/example/wallora/repository/collections/ChibiDragonsCollection.kt
@@ -15,6 +15,86 @@ object ChibiDragonsCollection {
             downloads = "New",
             badge = "NEW",
             isTopPick = true
+        ),
+        Wallpaper(
+            name = "Mushroom Dragon",
+            image = R.drawable.chibi_dragon_02,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Snow Dragon",
+            image = R.drawable.chibi_dragon_03,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ), Wallpaper(
+            name = "Sakura Dragon",
+            image = R.drawable.chibi_dragon_04,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Ocean Dragon",
+            image = R.drawable.chibi_dragon_05,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Autumn Dragon",
+            image = R.drawable.chibi_dragon_06,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Thunder Dragon",
+            image = R.drawable.chibi_dragon_07,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Honey Dragon",
+            image = R.drawable.chibi_dragon_08,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Galaxy Dragon",
+            image = R.drawable.chibi_dragon_09,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
+        ),
+        Wallpaper(
+            name = "Halloween Dragon",
+            image = R.drawable.chibi_dragon_10,
+            category = "Fantasy",
+            rating = 5.0,
+            downloads = "New",
+            badge = "NEW",
+            isTopPick = true
         )
 
     )
