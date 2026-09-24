68af9ed8b08b7e75b40ebd5a2cb1e5417eaca5f7
Nikola Lawniczak <nikola.lawniczak1991@gmail.com>
2026-09-24 13:28:45 +0200

    Bump version to 1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         minSdk = 24
         targetSdk = 36
 
-        versionCode = 13
-        versionName = "1.3.2"
+        versionCode = 14
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
